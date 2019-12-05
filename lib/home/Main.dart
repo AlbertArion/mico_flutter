a7759ae5c1b91996de1e1ce8ae4609f679aaf8c5
Albert <gaohaoran@xiaomi.com>
2019-12-05 21:13:28 +0800

    [Feature] Named routes.
    N/A
    
    Signed-off-by: Albert <gaohaoran@xiaomi.com>

--- lib/home/Main.dart
+++ lib/home/Main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mico_flutter/detail/DetailPage.dart';
 import 'package:mico_flutter/home/HomePage.dart';
 
 void main() => runApp(MyApp());
@@ -8,19 +9,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'MICO',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.red,
-        ),
-        home: MyHomePage());
+      title: 'MICO',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(),
+      initialRoute: '/',
+      routes: const <String, WidgetBuilder>{}
+    );
   }
+}
+
+class NamedRoutes {
+  static Map<String, WidgetBuilder> initRoutes() => {
+        '/detail': (context, {listItem, heroTag}) =>
+            DetailPage(listItem, heroTag)
+      };
 }
